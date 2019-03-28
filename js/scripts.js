d438dce7c411f1935a6ea3cab6e4e36453d6d72a
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-03-28 16:23:37 +0300

    created logic for scalene triangle

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,16 @@ function triTracker(sides) {
   var sideThree = document.getElementById("input3").value;
   var text;
 
-  var sides = [sideOne, sideTwo, sideThree].sort();
+  var sides = [sideOne, sideTwo, sideThree].sort((a, b) => a - b);
 
   // if(sides[0]===sides[1]&&sides[1]===sides[2]){
   //   text = "Your triangle is Equilateral!";
-  if(sides[0]===sides[1]||sides[1]===sides[2]){
-     text = "Your triangle is Isosceles!";
-   }
+  // if(sides[0]===sides[1]||sides[1]===sides[2]){
+  //    text = "Your triangle is Isosceles!";
+  //  }
+  if(sides[0]+sides[1]>sides[2]){
+   text = "Your triangle is Scalene!";
+  }
 
   document.getElementById("triType").innerHTML = text;
 }
