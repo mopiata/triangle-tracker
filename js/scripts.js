b83b62b12096308df69c549cf5bfc84ae7d407b2
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-03-28 15:41:48 +0300

    created logic for equilateral triangle

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function triTracker(sides) {
+  var sideOne = document.getElementById("input1").value;
+  var sideTwo = document.getElementById("input2").value;
+  var sideThree = document.getElementById("input3").value;
+  var text;
+
+  var sides = [sideOne, sideTwo, sideThree].sort();
+
+  if(sides[0]===sides[1]&&sides[1]===sides[2]){
+    text = "Your triangle is Equilateral!";
+}
+
+  document.getElementById("triType").innerHTML = text;
+}
