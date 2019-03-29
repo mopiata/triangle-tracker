a05cdb5e6b76e73793b4d739ce9439201595b928
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-03-29 09:37:25 +0300

    added functionality to check for null, zero and negative numbers

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,20 @@ function triTracker() {
 
   var sides = [sideOne, sideTwo, sideThree].sort((a, b) => a - b);
   
-  if(sides[0]===sides[1]&&sides[1]===sides[2]){
-    text = "Your triangle is Equilateral!";
-  }else if(sides[0]===sides[1]||sides[1]===sides[2]){
-     text = "Your triangle is Isosceles!";
-   }else if(sides[0]+sides[1]>sides[2]){
-   text = "Your triangle is Scalene!";
+  if (sides[0] > 0 && sides[1] > 0 && sides[2] > 0){
+    if (sides[0] === sides[1] && sides[1] === sides[2]){
+      text = "Your triangle is Equilateral!";
+    } else if (sides[0] === sides[1] || sides[1] === sides[2]){
+      text = "Your triangle is Isosceles!";
+    } else if (sides[0] + sides[1] > sides[2]){
+    text = "Your triangle is Scalene!";
+    }else{
+      text ="Your values cannot form a triangle!";
+    }
   }else{
-    text ="Your values cannot form a triangle!";
+    text="You're required to enter a positive number!";
   }
 
   document.getElementById("triType").innerHTML = text;
 }
+
